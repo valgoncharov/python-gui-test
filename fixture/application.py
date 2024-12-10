6be1643b1986855a7bddabb1fdaab25a92e51f71
valgoncharov <99090572+valgoncharov@users.noreply.github.com>
2024-12-10 22:20:55 +0300

    Add test
    Test add new group

--- fixture/application.py
+++ fixture/application.py
@@ -1,11 +1,15 @@
 from pywinauto.application import Application as WinApplication
+from fixture.group import GroupHelper
 
 
 class Application:
 
     def __init__(self, target):
         self.application = WinApplication(backend="win32").start(target)
+        self.main_window = self.application.window(title="Free Address Book")
+        self.main_window.wait("visible")
+        self.groups = GroupHelper(self)
 
     def destroy(self):
-        pass
+        self.main_window.close()
 
